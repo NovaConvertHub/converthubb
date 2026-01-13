f5d55a2c2359fffa3bae59f2cea9fcc3692b23c9
NovaConvertHub <mb5439102@gmail.com>
2026-01-13 04:52:54 +0100

    Update text-to-audio.js

--- api/text-to-audio.js
+++ api/text-to-audio.js
@@ -1,4 +1,4 @@
-const { gTTS } = require("gtts.js");
+const GTTS = require("gtts");
 
 module.exports = async (req, res) => {
   if (req.method !== "POST") return res.status(405).send("Method not allowed");
@@ -7,12 +7,14 @@ module.exports = async (req, res) => {
   if (!text) return res.status(400).send("No text provided");
 
   try {
-    const speech = new gTTS(text, "en");
-    const buffer = await speech.saveToBuffer();
+    const gtts = new GTTS(text, "en");
 
+    // stream directly to response
     res.setHeader("Content-Type", "audio/mpeg");
     res.setHeader("Content-Disposition", "attachment; filename=output.mp3");
-    res.send(Buffer.from(buffer));
+
+    gtts.stream().pipe(res);
+
   } catch (err) {
     console.error(err);
     res.status(500).send("Error generating audio");
